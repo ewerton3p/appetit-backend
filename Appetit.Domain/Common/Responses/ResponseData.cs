85bb1a878122690ef3e16f3994575e590261907d
Ewerton Lauredo <ewerton3p@gmail.com>
2026-04-05 13:18:15 -0300

    ResponseData retorna true por padrão

--- Appetit.Domain/Common/Responses/ResponseData.cs
+++ Appetit.Domain/Common/Responses/ResponseData.cs
@@ -4,7 +4,7 @@
     {
         public T? Data { get; set; }
         public string Message { get; set; } = string.Empty;
-        public bool Success { get; set; }
+        public bool Success { get; set; } = true;
         public Pagination? Pagination { get; set; }
         public string? Details { get; set; }
 
